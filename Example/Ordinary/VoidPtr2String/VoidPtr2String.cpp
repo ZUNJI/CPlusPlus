298cb1b1582030686cea9f95dfbaa9d9e61c6448
Shell Lee <toothing@foxmail.com>
2016-07-20 19:41:08 +0800

    1、方案改变

--- Example/Ordinary/VoidPtr2String/VoidPtr2String.cpp
+++ Example/Ordinary/VoidPtr2String/VoidPtr2String.cpp
@@ -3,38 +3,59 @@
 #include <iostream>
 //#include <string>
 #include <sstream>
+#include <iomanip>
 
 std::string voidPtr2String( const void* ptr )
 {
 	std::ostringstream obuffer;
-	obuffer<<ptr;
+	obuffer << std::setw( sizeof( ptr ) * 2 ) << std::hex << std::setfill( '0' ) << std::setiosflags( std::ios::uppercase ) << (unsigned long long)( ptr );
 
-	return obuffer.str();
+	std::string address = obuffer.str();
+
+	obuffer.str( "" );
+	obuffer.clear();
+
+	return address;
 }
 
-void* string2VoidPtr( const std::string& ptr )
+void* string2VoidPtr( const std::string& address )
 {
-	void* voidPtr;
-	std::istringstream ibuffer(ptr);
-	ibuffer >> voidPtr;
+	std::istringstream ibuffer( address );
 
-	return voidPtr;
+	unsigned long long ptr = 0x0;
+	ibuffer >> std::hex >> ptr;
+
+	ibuffer.str( "" );
+	ibuffer.clear();
+
+	return (void*)( ptr );
 }
 
 int main( int argc, char** argv )
 {
-	void* vptr = (void*)0xAAAAAAAAAAAAAAAA;
-	std::string svptr = voidPtr2String( vptr );
-	//delete (int*)vptr;
+	for( int i = 0; i < 300; i++ )
+	{
+		void* vptr = (void*)0xAAAAAAAAAAAAAAAA;
+		std::string svptr = voidPtr2String( vptr );
+		//delete (int*)vptr;
 
-	std::string svptr2 = "0x5555555555555555";
-	void* vptr2 = string2VoidPtr( svptr2 );
+		std::string svptr2 = "0x5555555555555555";
+		void* vptr2 = string2VoidPtr( svptr2 );
 
-	std::cout << svptr << std::endl;
-	std::cout << vptr2 << std::endl;
+		std::cout << svptr << std::endl;
+		std::cout << vptr2 << std::endl;
 
-	int* iptr = new int;
-	delete iptr;
-	std::string siptr = voidPtr2String( (void*)iptr );
-	std::cout << siptr << std::endl;
+		int* iptr = new int;
+		*iptr = 123456;
+		std::string siptr = voidPtr2String( (void*)iptr );
+		std::cout << siptr << std::endl;
+
+		void* viptr = string2VoidPtr( siptr );
+		std::cout << viptr << std::endl;
+		std::cout << *(int*)viptr << std::endl;
+
+		delete iptr;
+	}
+
+	return 0;
 }
